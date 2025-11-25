31626054feb453f309b3b3dacd1fc8bc611c33d1
Ssang-tuk <goo3735@tukorea.ac.kr>
2025-11-25 11:55:23 +0900

    Drill #12 - 2024182025_이승구

--- zombie.py
+++ zombie.py
@@ -74,6 +74,7 @@ class Zombie:
 
 
         draw_rectangle(*self.get_bb())
+        draw_circle(self.x, self.y, int(7.0 * PIXEL_PER_METER), 1, 1, 1)
 
     def handle_event(self, event):
         pass
@@ -141,6 +142,11 @@ class Zombie:
         self.loc_no = (self.loc_no + 1) % len(self.patrol_locations)
         return BehaviorTree.SUCCESS
 
+    def have_more_balls(self):
+        if self.ball_count >= common.boy.ball_count:
+            return BehaviorTree.SUCCESS
+        else:
+            return BehaviorTree.FAIL
 
     def build_behavior_tree(self):
         a1 = Action('Set target location', self.set_target_location, 1000, 1000)
@@ -149,14 +155,18 @@ class Zombie:
         a3 = Action('Set random location', self.set_random_location)
         root = wander = Sequence('Wander', a3, a2)
 
+        c_have_more = Condition('좀비가 공 >= 소년?', self.have_more_balls)
+
         c1 = Condition('소년이 근처에 있는가?', self.is_boy_nearby, 7)
         a4 = Action('소년한테 접근', self.move_to_boy)
-        root = chase_boy = Sequence('소년을 추적', c1, a4)
-
-        root = chase_or_flee = Selector('추적 또는 배회', chase_boy, wander)
+        root = chase_boy_if_nearby = Sequence('소년을 추적', c_have_more, a4)
 
         a5 = Action('순찰 위치 가져오기', self.get_patrol_location)
-        root = patrol = Sequence('순찰', a5, a2)
+        # root = patrol = Sequence('순찰', a5, a2)
+
+        # root = chase_if_boy_near_or_patrol = Selector('추적 또는 순찰', chase_boy_if_nearby, patrol)
+        root = Selector('행동', Sequence('근접 검사 후 행동', c1, chase_boy_if_nearby), wander)
+
 
         self.bt = BehaviorTree(root)
 
